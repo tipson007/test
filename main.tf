4c0a55f704dde8a86774ff635bd2f374b6d0ad10
Samuel O T <kooldec@yahoo.co.uk>
2024-04-18 09:51:56 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+# The configuration for the `remote` backend.
+terraform {
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "Cloud-Platform"
+
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "sam-aws-workspace"
+    }
+  }
+}
+
+# An example resource that does nothing.
+resource "null_resource" "example" {
+  triggers = {
+    value = "An example resource that does nothing!"
+  }
+}
